4678064c6f317854e8eec313e0c1be4106ea73fd
Tricodando <85141942+tricodando@users.noreply.github.com>
2024-06-05 14:13:51 -0300

    Delete capitulo-4 directory

--- capitulo-4/questao-9.py
+++
@@ -1,8 +0,0 @@
-"""
-autor: Reuel Caetano
-título: Lógica de Programação
-subtítulo: Programando de forma moderna com Python 3
-instruções: Execute diretamente no shell do Python
-"""
-
-2 ** (3 ** 3)
