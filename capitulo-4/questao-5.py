0aae177b8dbd5d90629e4c6c326e8051de0ffbed
Tricodando <85141942+tricodando@users.noreply.github.com>
2024-06-05 14:11:33 -0300

    chapter 4 solutions

---
+++ capitulo-4/questao-5.py
@@ -0,0 +1,10 @@
+"""
+autor: Reuel Caetano
+título: Lógica de Programação
+subtítulo: Programando de forma moderna com Python 3
+instruções: Execute diretamente no shell do Python
+"""
+
+10 / 2   # letra a
+3 / 1    # letra b
+5 / 0.5  # letra c
